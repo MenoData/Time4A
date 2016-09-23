70407afe9f374330c2faed004e60bfd6c44de792
Meno Hochschild <mhochschild@gmx.de>
2016-09-23 06:48:47 +0200

    new version label

--- time4j-android/src/main/java/net/time4j/android/ApplicationStarter.java
+++ time4j-android/src/main/java/net/time4j/android/ApplicationStarter.java
@@ -60,10 +60,10 @@ public class ApplicationStarter {
 
     //~ Statische Felder/Initialisierungen --------------------------------
 
-    private static final String VERSION = "v3.19-2016f";
+    private static final String VERSION = "v3.23-2016g";
     private static final int RELEASE_YEAR = 2016;
-    private static final int RELEASE_MONTH = 7;
-    private static final int RELEASE_DAY = 6;
+    private static final int RELEASE_MONTH = 9;
+    private static final int RELEASE_DAY = 23;
     private static final String TIME4A = "time4a";
 
     private static final AtomicBoolean PREPARED = new AtomicBoolean(false);
