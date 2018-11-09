589c786d30c18f16421b4b922d2ed18b1d0808ff
Meno Hochschild <mhochschild@gmx.de>
2018-11-09 05:20:18 +0100

    javadoc update

--- time4j-android/src/main/java/net/time4j/calendar/ChineseCalendar.java
+++ time4j-android/src/main/java/net/time4j/calendar/ChineseCalendar.java
@@ -130,7 +130,7 @@ import java.util.Map;
  *       is(chineseDate));
  *     assertThat(
  *       formatter.format(chineseDate),
- *       is(&quot;Sat, 16. M11 2018(wù-xū) dōngzhì&quot;));
+ *       is(&quot;Sat, 16. Eleventh Month 2018(wù-xū) dōngzhì&quot;));
  * </pre>
  *
  * <p>Leap months can be formatted in various ways. Following example shows how to customize numerical printing
@@ -221,7 +221,7 @@ import java.util.Map;
  *       is(chineseDate));
  *     assertThat(
  *       formatter.format(chineseDate),
- *       is(&quot;Sat, 16. M11 2018(wù-xū) dōngzhì&quot;));
+ *       is(&quot;Sat, 16. Eleventh Month 2018(wù-xū) dōngzhì&quot;));
  * </pre>
  *
  * <p>Schaltmonate k&ouml;nnen verschieden formatiert werden. Das folgende Beispiel zeigt, wie man
